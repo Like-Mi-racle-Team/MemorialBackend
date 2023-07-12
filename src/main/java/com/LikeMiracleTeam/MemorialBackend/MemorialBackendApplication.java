23e45a351c254647e55d2317f7aaf3d1fb2d18bb
unavailableusername09 <legeno09133@gmail.com>
2023-07-12 09:40:01 +0900

    project init

---
+++ src/main/java/com/LikeMiracleTeam/MemorialBackend/MemorialBackendApplication.java
@@ -0,0 +1,13 @@
+package com.LikeMiracleTeam.MemorialBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorialBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemorialBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LikeMiracleTeam/MemorialBackend/MemorialBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LikeMiracleTeam.MemorialBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorialBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
